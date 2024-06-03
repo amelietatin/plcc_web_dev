2f5ce4754455954dc6f701611f977e1a8b10ae66
Ali Shishehgar <alishishegar@gmail.com>
2024-06-03 09:33:40 +0200

    The first version of the live API URL is ready

--- api/main.py
+++ api/main.py
@@ -9,23 +9,28 @@ app = FastAPI()
 df = pd.read_csv('api/final_data_2015_2035.csv')
 #load_data_to_bq(df, GCP_PROJECT, BQ_DATASET, TABLE, True)
 
-query = f"""
-        SELECT *
-        FROM `{GCP_PROJECT}.{BQ_DATASET}.{TABLE}`
-        LIMIT 100
-    """
-df_bq = get_data(GCP_PROJECT, query)
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the CSV API"}
 
 @app.get("/data")
-def get_data():
+def data():
+    query = f"""
+        SELECT *
+        FROM `{GCP_PROJECT}.{BQ_DATASET}.{TABLE}`
+    """
+    df_bq = get_data(GCP_PROJECT, query)
     return df_bq.to_dict(orient='records')
 
 @app.get("/data/{item_id}")
 def get_data_item(item_id: str):
+    query = f"""
+        SELECT *
+        FROM `{GCP_PROJECT}.{BQ_DATASET}.{TABLE}`
+        WHERE SITECODE IS {item_id}
+    """
     # if item_id >= len(df):
     #     return {"error": "Item not found"}
-    return df_bq.loc[df['SITECODE'] == item_id].to_dict()
+    df_bq = get_data(GCP_PROJECT, query)
+    return df_bq.to_dict(orient='records')
