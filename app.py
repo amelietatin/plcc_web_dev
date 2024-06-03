d0ff92bef2e31274612f5c50119969c626e710ae
Amelie Tatin <amelietatin2@gmail.com>
2024-06-03 16:28:05 +0200

    several changes in the app

--- app.py
+++ app.py
@@ -1,137 +1,292 @@
+
+from io import StringIO ## for Python 3
 import streamlit as st
-import os
 import pandas as pd
-import geopandas as gpd
 import folium
 from streamlit_folium import st_folium
+import ee
+import geemap.geemap as geemap
+import geopandas as gpd
+from io import StringIO
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import geopandas as gpd
 
-
+import requests
 
 # Page configuration
-st.set_page_config(page_title="Land Cover Changes Predictor", page_icon="🔍", layout="centered")
+st.set_page_config(page_title="Land Cover Change Predictions", page_icon="🔍", layout="centered")
 
-#Import protected areas shapefile
-protected_areas = gpd.read_file("../raw_data/sample_protected_areas_624/protected_areas_624.shp")
-protected_areas_sample = protected_areas.iloc[[0]]
+#############################################################################################################################################
+#############################################################################################################################################
+# DATA
 
-# Create the Folium map
-def create_folium_map(gdf):
-    # Create a base map
-    m = folium.Map(location=[gdf.geometry.centroid.y.mean(), gdf.geometry.centroid.x.mean()], zoom_start=10)
-    # Add the shapefile to the map
-    folium.GeoJson(protected_areas_sample).add_to(m)
-    return m
-folium_map = create_folium_map(protected_areas_sample)
-# Display the Folium map in Streamlits
-st_folium(folium_map, width=700, height=500)
+# Load data
+df = pd.read_csv('raw_data/final_data_2015_2035.csv')
+
+# Timerange
+date_range_df = pd.read_csv('raw_data/date_ranges.csv', sep=',')
+
+#pa shapefile
+pa_sample = gpd.read_file("raw_data/sample_protected_areas_624/protected_areas_624.shp")
+
+## GEE SERVICE ACCOUNT
+service_account = 'project-lc@lewagon-lc-amelietatin.iam.gserviceaccount.com'
+credentials = ee.ServiceAccountCredentials(service_account, './key.json')
+ee.Initialize(credentials)
+
+#Import protected areas GEE asset
+shapefile = ee.FeatureCollection("projects/lewagon-lc-amelietatin/assets/sample_protected_areas_624")
 
 
-# Upload CSV
-df = pd.read_csv('raw_data/Final_df_model_lc_2015_2024.csv')
 
-# Custom CSS for styling
-st.markdown("""
-    <style>
-    .main-title {
-        font-size: 2.5rem;
-        color: black;
-        text-align: center;
-        margin-bottom: 20px;
-        line-height: 2.5; /* Adjust line height */
+#############################################################################################################################################
+#############################################################################################################################################
+# DROPDOWNS
+
+#SITECODES
+sitecodes = pa_sample['SITECODE'].unique()
+selected_sitecode = st.selectbox(
+    label='Sitecode:',
+    options=sitecodes,
+    index=0,
+    help='Select a Sitecode',
+)
+
+#YEARS
+years = date_range_df['Year'].unique()
+selected_year = st.selectbox(
+        label='Year:',
+        options=list(years),
+        index=0,
+        help='Select a Year',
+)
+
+#QUARTERS
+quarters = {'Q1': '01-01', 'Q2': '04-01', 'Q3': '07-01', 'Q4': '10-01'}
+#quarters = date_range_df['Quartal'].unique()
+#Create dropdown for quarters
+quarter_dropdown = st.selectbox(
+    label='Quarter:',
+    options=list(quarters.keys()),
+    index=0,
+    help='Select a Quarter',
+    )
+
+#############################################################################################################################################
+#############################################################################################################################################
+# AMELIE
+#############################################################################################################################################
+#############################################################################################################################################
+
+#Define which quartal
+start = date_range_df[(date_range_df['Quartal'] == quarter_dropdown) & (date_range_df['Year'] == selected_year)]['Start_Date'].values[0]
+end = date_range_df[(date_range_df['Quartal'] == quarter_dropdown) & (date_range_df['Year'] == selected_year)]['End_Date'].values[0]
+
+with st.container():
+    st.markdown('<h2 style="font-size:24px;">Part I: Land Cover Proportion by Quarter</h2>', unsafe_allow_html=True)
+    st.markdown("This section will display the change in land cover over time using images.")
+
+    # Convert start and end dates to ee.Date
+    START = ee.Date(str(start))
+    END = ee.Date(str(end))
+
+    # Filter the shapefile for the specific protected area
+    specific_feature_collection = shapefile.filter(ee.Filter.eq('SITECODE', selected_sitecode))
+    geometry = specific_feature_collection.geometry()
+
+    # Create a filter based on the geometry and date range
+    col_filter = ee.Filter.And(
+        ee.Filter.geometry(geometry),
+        ee.Filter.date(START, END),
+    )
+
+    # Apply the filter to the DynamicWorld and Sentinel-2 collections
+    dw_col = ee.ImageCollection('GOOGLE/DYNAMICWORLD/V1').filter(col_filter).filterBounds(geometry)
+    dw_col_clipped = dw_col.map(lambda image: image.clip(geometry))
+    s2_col = ee.ImageCollection('COPERNICUS/S2').filter(col_filter)
+
+    # Join corresponding DW and S2 images
+    dw_s2_col = ee.Join.saveFirst('s2_img').apply(
+        dw_col_clipped,
+        s2_col,
+        ee.Filter.equals(leftField='system:index', rightField='system:index'),
+    )
+
+    # Extract an example DW image and its source S2 image
+    dw_image = ee.Image(dw_s2_col.first())
+    s2_image = ee.Image(dw_image.get('s2_img'))
+
+    # Define land cover class names and their corresponding color palette
+    CLASS_NAMES = [
+        'water', 'trees', 'grass', 'flooded_vegetation', 'crops',
+        'shrub_and_scrub', 'built', 'bare', 'snow_and_ice'
+    ]
+    VIS_PALETTE = [
+        '419bdf', '397d49', '88b053', '7a87c6', 'e49635',
+        'dfc35a', 'c4281b', 'a59b8f', 'b39fe1'
+    ]
+
+    # Create an RGB image of the label (most likely class) on [0, 1]
+    dw_rgb = (
+        dw_image.select('label')
+        .visualize(min=0, max=8, palette=VIS_PALETTE)
+        .divide(255)
+    )
+
+    # Get the most likely class probability
+    top1_prob = dw_image.select(CLASS_NAMES).reduce(ee.Reducer.max())
+
+    # Create a hillshade of the most likely class probability on [0, 1]
+    top1_prob_hillshade = ee.Terrain.hillshade(top1_prob.multiply(100)).divide(255)
+
+    # Combine the RGB image with the hillshade
+    dw_rgb_hillshade = dw_rgb.multiply(top1_prob_hillshade)
+
+    # Get lon and lat of protected area
+    dict_pa = specific_feature_collection.getInfo()
+    lon = dict_pa.get('features')[0].get('properties').get('lon')
+    lat = dict_pa.get('features')[0].get('properties').get('lat')
+
+    # Initialize map
+    m = geemap.Map(center=(lat, lon), zoom=14)
+
+    # Add protected area layer
+    m.addLayer(geometry, {}, 'Protected Area')
+
+    # Add Dynamic World layer
+    m.addLayer(dw_rgb_hillshade, {'min': 0, 'max': 0.65}, 'Dynamic World Dataset')
+
+
+    # Display map in Streamlit
+    m.to_streamlit(height=600)
+
+
+#############################################################################################################################################
+#############################################################################################################################################
+# FLORI
+#############################################################################################################################################
+#############################################################################################################################################
+
+
+# url_base='https://landcoverapi-f37abimraq-ew.a.run.app/'
+# response = requests.get(url=url_base+'/data').json()
+# df = pd.DataFrame(response)
+
+#Drop 'Unnamed: 0' column if it exists (as I haad this problem before)
+if 'Unnamed: 0' in df.columns:
+    df = df.drop(columns=['Unnamed: 0'])
+
+df_lc = df.drop(columns=['temperature_quarterly_mean', 'precipitation_quarterly_mean', 'water-vapor-pressure_quarterly_mean', 'cloud-cover_quarterly_mean'])
+df_eco = df[["SITECODE", "quarter_start", "temperature_quarterly_mean", "precipitation_quarterly_mean", "water-vapor-pressure_quarterly_mean", "cloud-cover_quarterly_mean"]].copy()
+
+# Integration of graph code
+df_lc['quarter_start'] = pd.to_datetime(df_lc['quarter_start'])
+df_eco['quarter_start'] = pd.to_datetime(df_lc['quarter_start'])
+
+
+with st.container():
+    st.markdown('<h2 style="font-size:24px;">Part II: Predictions of Change in Land Cover Proportions over time</h2>', unsafe_allow_html=True)
+    st.markdown("This section will display the predicted change over the next 10 years.")
+
+
+    # Define color dictionary for land cover categories
+    color_dict = {
+        'Trees': '#397d49',
+        'Snow and Ice': '#b39fe1',
+        'Water': '#419bdf',
+        'Bare Ground': '#a59b8f',
+        'Crops': '#e49635',
+        'Grass': '#88b053',
+        'Shrub and Scrub': '#dfc35a',
+        'Built Area': '#c4281b',
+        'Flooded Vegetation': '#7a87c6'
     }
-    .subtitle {
-        font-size: 1.5rem;
-        color: black;
-        text-align: center;
-        margin-bottom: 20px;
-        line-height: 2.5; /* Adjust line height */
-    }
-    .footer {
-        position: fixed;
-        left: 0;
-        bottom: 0;
-        width: 100%;
-        background-color: rgba(232, 234, 235, 0.8);
-        text-align: center;
-        padding: 10px;
-        font-size: 14px;
-        color: #333;
-    }
-    .success {
-        color: green;
-    }
-    .error {
-        color: red;
-    }
-    .stButton button {
-        background-color: #4A90E2;
-        color: white;
-    }
-    .centered-button {
-        text-align: center;
-    }
-    </style>
-    """, unsafe_allow_html=True)
 
-# Title and subtitle
-st.markdown('<div class="main-title">Land Cover Changes Predictor 🔍</div>', unsafe_allow_html=True)
-st.markdown('<div class="subtitle">Check Land Cover Status Between 2015 and 2024</div>', unsafe_allow_html=True)
 
-# Sample list of existing IDs
-existing_ids = ["40A0", "91D0", "91E0", "91F0", "91U0", "1130", "1140", "1210", "6240", "6170"]
+def update_df(sitecode, quarter):
+    filtered_pa_data = df_lc[df_lc['SITECODE'] == sitecode]
+    start_date = quarters[quarter]
+    quarter_dates = filtered_pa_data[filtered_pa_data['quarter_start'].dt.strftime('%m-%d') == start_date]
+    return quarter_dates
 
-# Text input for ID search
-user_input = st.text_input("Enter the protected area's ID and press Enter:")
+quarter_dates = update_df(selected_sitecode, quarter_dropdown)
 
-# Check if the input ID is in the list
-if user_input:
-    if user_input in existing_ids:
-        st.markdown(f"<span class='success'>ID {user_input} found!</span>", unsafe_allow_html=True)
-    else:
-        st.markdown(f"<span class='error'>Error: ID {user_input} not found.</span>", unsafe_allow_html=True)
+# change negative values for lc proportions to 0
+quarter_dates.iloc[:, 3:] = quarter_dates.iloc[:, 3:].clip(lower=0)
 
-# Year selection slider
-year = st.slider("Select Year", 2015, 2024, 2015)
+def update_plot(quarter_dates, sitecode, quarter):
+    fig, ax = plt.subplots(figsize=(16, 8))
 
-# Display map for the selected ID and year if the ID is valid
-if user_input and user_input in existing_ids:
-    # Assuming you have map images stored in a folder named 'maps'
-    # with filenames formatted as 'ID_Year.png' (e.g., '40A0_2015.png')
-    map_filename = f"maps/{user_input}_{year}.png"
+    # Set the Seaborn style
+    sns.set(style="whitegrid")
 
-    if os.path.exists(map_filename):
-        st.image(map_filename, caption=f"Land Cover Map for {user_input} in {year}", use_column_width=True)
-    else:
-        st.markdown(f"<span class='error'>Map not found for ID {user_input} in {year}.</span>", unsafe_allow_html=True)
+    # Get colors for the categories from the color dictionary, falling back to default if not found
+    colors = [color_dict.get(col, 'grey') for col in quarter_dates.columns if col not in ['quarter_start', 'SITECODE']]
 
-st.markdown('<div class="subtitle">Predict The Status of The Land Cover in The Future</div>', unsafe_allow_html=True)
+    quarter_dates.set_index('quarter_start').plot(kind='area', stacked=True, color=colors, alpha=0.8, ax=ax)
+    ax.set_title(f'Land Cover Proportions Over the Years for {sitecode} - {quarter}', fontsize=18)
+    ax.set_xlabel('Year', fontsize=16)
+    ax.set_ylabel('Proportion', fontsize=16)
+    ax.tick_params(axis='x', labelsize=16)
+    ax.tick_params(axis='y', labelsize=16)
+    ax.legend(title='Land Cover Category', bbox_to_anchor=(0, -0.15), ncol = 2, loc='upper left', fontsize=16, title_fontsize='18')
+    ax.grid(False)
 
-# Form for year, temperature, and precipitation inputs
-with st.form(key='prediction_form'):
-    st.markdown('<div class="subtitle">Enter Prediction Parameters</div>', unsafe_allow_html=True)
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        pred_year = st.text_input("Year")
-    with col2:
-        temperature = st.text_input("Temperature (°C)")
-    with col3:
-        precipitation = st.text_input("Precipitation (mm)")
+    ax.axvline(pd.to_datetime('2025-01-01'), color='r', linestyle='--')
 
-    # Center the submit button
-    st.markdown('<div class="centered-button">', unsafe_allow_html=True)
-    submit_button = st.form_submit_button(label='Submit')
-    st.markdown('</div>', unsafe_allow_html=True)
+    # Return the Matplotlib figure object
+    return fig
 
-# Display prediction result after form submission
-if submit_button:
-    if pred_year and temperature and precipitation:
-        st.markdown(f"### Here is the predicted status of land cover in {pred_year} with the given temperature ({temperature}°C) and precipitation ({precipitation} mm).")
-    else:
-        st.markdown(f"<span class='error'>Please fill in all the fields.</span>", unsafe_allow_html=True)
+fig = update_plot(quarter_dates, selected_sitecode, quarter_dropdown)
+
+# Display the plot in Streamlit
+st.pyplot(fig)
+
+
+# Additional graphs for temperature, precipitation, water-vapor-pressure, and cloud-cover:
+with st.container():
+    import plotly.graph_objects as go
+
+    # Filter the data for the selected sitecode and quarter
+    quarter_data = df_eco[df_eco['SITECODE'] == selected_sitecode]
+    quarter_data = quarter_data[quarter_data['quarter_start'].dt.quarter == int(quarter_dropdown[1])]
+
+    # Create the figure
+    fig = go.Figure()
+
+    # Add traces for each ecological variable
+    fig.add_trace(go.Scatter(x=quarter_data['quarter_start'], y=quarter_data['temperature_quarterly_mean'], mode='lines', name='Temperature', line=dict(color='red')))
+    fig.add_trace(go.Scatter(x=quarter_data['quarter_start'], y=quarter_data['precipitation_quarterly_mean'], mode='lines', name='Precipitation', line=dict(color='blue')))
+    fig.add_trace(go.Scatter(x=quarter_data['quarter_start'], y=quarter_data['water-vapor-pressure_quarterly_mean'], mode='lines', name='Water Vapor Pressure', line=dict(color='green')))
+    fig.add_trace(go.Scatter(x=quarter_data['quarter_start'], y=quarter_data['cloud-cover_quarterly_mean'], mode='lines', name='Cloud Cover', line=dict(color='orange')))
+
+    # Update layout to add hover mode and legend positioning
+    fig.update_layout(
+        hovermode='x', legend=dict(x=0, y=-0.65), xaxis_title='Year', yaxis_title='Value',
+        xaxis=dict(tickmode='array', tickvals=quarter_data['quarter_start'], tickformat="%Y")
+        )
+
+    #add line in 2025 to mark the start of predicted values
+    fig.add_shape(
+        type="line",
+        x0="2025",
+        y0=0,
+        x1="2025",
+        y1=1,
+        line=dict(color="red", width=2, dash="dash"),
+        xref="x",
+        yref="paper"
+    )
+
+    # Show the plot
+    st.plotly_chart(fig)
+
 
 # Add footer
 st.markdown("""
     <div class="footer">
-        Created with ❤️ by Amélie, Tim, Florentine, and Ali
+        Created with ❤️ by Amélie, Tim, Flori, and Ali
     </div>
     """, unsafe_allow_html=True)
